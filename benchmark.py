7ff2cc62f55796b61dd0360cf41ba6a7bf0eb160
Ben Hoyt <benhoyt@gmail.com>
2012-11-22 22:45:16 +1300

    Made benchmark.py create test tree by default

--- benchmark.py
+++ benchmark.py
@@ -1,10 +1,38 @@
 """Simple benchmark to compare the speed of BetterWalk with os.walk()."""
 
+import optparse
 import os
 import sys
 import timeit
 
 import betterwalk
+
+# TODO: run each benchmark a couple of times (in different orders), take the min, then compare
+# TODO: better printing out of results
+
+DEPTH = 4
+NUM_DIRS = 5
+NUM_FILES = 50
+
+def create_tree(path, depth=DEPTH):
+    """Create a directory tree at path with given depth, and NUM_DIRS and
+    NUM_FILES at each level.
+    """
+    os.mkdir(path)
+    for i in range(NUM_FILES):
+        filename = os.path.join(path, 'file{0:03}.txt'.format(i))
+        with open(filename, 'wb') as f:
+            line = 'The quick brown fox jumps over the lazy dog.\n'
+            if i == 0:
+                # So we have at least one big file per directory
+                f.write(line * 20000)
+            else:
+                f.write(line * i * 10)
+    if depth <= 1:
+        return
+    for i in range(NUM_DIRS):
+        dirname = os.path.join(path, 'dir{0:03}'.format(i))
+        create_tree(dirname, depth - 1)
 
 def benchmark(path):
     def do_os_walk():
@@ -24,5 +52,26 @@ def benchmark(path):
     print('os.walk took {0:.3}s, BetterWalk took {1:.3}s -- {2:.2}x as fast'.format(
           os_walk_time, betterwalk_time, os_walk_time / betterwalk_time))
 
+def main():
+    """Usage: benchmark.py [-h] [tree_dir]
+
+Create 230MB directory tree named "benchtree" (relative to this script) and
+benchmark os.walk() versus betterwalk.walk(). If tree_dir is specified,
+benchmark using it instead of creating a tree.
+"""
+    parser = optparse.OptionParser(usage=main.__doc__.rstrip())
+    options, args = parser.parse_args()
+
+    if args:
+        tree_dir = args[0]
+    else:
+        tree_dir = os.path.join(os.path.dirname(__file__), 'benchtree')
+        if not os.path.exists(tree_dir):
+            print 'Creating tree at {0}: depth={1}, num_dirs={2}, num_files={3}'.format(
+                tree_dir, DEPTH, NUM_DIRS, NUM_FILES)
+            create_tree(tree_dir)
+
+    benchmark(tree_dir)
+
 if __name__ == '__main__':
-    benchmark(sys.argv[1] if len(sys.argv) > 1 else '.')
+    main()
