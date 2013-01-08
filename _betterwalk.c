992cf7de5c89a6a4ba28775bfa206a2689ea81cd
Ben Hoyt <benhoyt@gmail.com>
2013-01-08 22:43:46 +1300

    Got C _betterwalk.listdir() proof of concept basically working

--- _betterwalk.c
+++ _betterwalk.c
@@ -15,29 +15,62 @@
 #endif
 
 static PyObject *
-win32_error(char* function, char* filename)
-{
-    /* XXX We should pass the function name along in the future.
-       (_winreg.c also wants to pass the function name.)
-       This would however require an additional param to the
-       Windows error object, which is non-trivial.
-    */
-    errno = GetLastError();
-    if (filename)
-        return PyErr_SetFromWindowsErrWithFilename(errno, filename);
-    else
-        return PyErr_SetFromWindowsErr(errno);
-}
-
-static PyObject *
 win32_error_unicode(char* function, Py_UNICODE* filename)
 {
-    /* XXX - see win32_error for comments on 'function' */
     errno = GetLastError();
     if (filename)
         return PyErr_SetFromWindowsErrWithUnicodeFilename(errno, filename);
     else
         return PyErr_SetFromWindowsErr(errno);
+}
+
+/* Below, we *know* that ugo+r is 0444 */
+#if _S_IREAD != 0400
+#error Unsupported C library
+#endif
+static int
+attributes_to_mode(DWORD attr)
+{
+    int m = 0;
+    if (attr & FILE_ATTRIBUTE_DIRECTORY)
+        m |= _S_IFDIR | 0111; /* IFEXEC for user,group,other */
+    else
+        m |= _S_IFREG;
+    if (attr & FILE_ATTRIBUTE_READONLY)
+        m |= 0444;
+    else
+        m |= 0666;
+    return m;
+}
+
+double
+filetime_to_time(FILETIME *filetime)
+{
+    const double SECONDS_BETWEEN_EPOCHS = 11644473600.0;
+
+    unsigned long long total = (unsigned long long)filetime->dwHighDateTime << 32 |
+                               (unsigned long long)filetime->dwLowDateTime;
+    return (double)total / 10000000.0 - SECONDS_BETWEEN_EPOCHS;
+}
+
+static PyObject *
+find_data_to_stat(WIN32_FIND_DATAW *data)
+{
+    PY_LONG_LONG size = (PY_LONG_LONG)data->nFileSizeHigh << 32 |
+                        (PY_LONG_LONG)data->nFileSizeLow;
+
+    PyObject* result = Py_BuildValue("(iiiiiiKddd)",
+        attributes_to_mode(data->dwFileAttributes), // st_mode
+        0,                                          // st_ino
+        0,                                          // st_dev
+        0,                                          // st_nlink
+        0,                                          // st_uid
+        0,                                          // st_gid
+        size,                                       // st_size
+        filetime_to_time(&data->ftLastAccessTime),  // st_atime,
+        filetime_to_time(&data->ftLastWriteTime),   // st_mtime,
+        filetime_to_time(&data->ftCreationTime));   // st_ctime,
+    return result;
 }
 
 static PyObject *
@@ -46,151 +79,94 @@ listdir(PyObject *self, PyObject *args)
     PyObject *d, *v;
     HANDLE hFindFile;
     BOOL result;
-    WIN32_FIND_DATA FileData;
-    char namebuf[MAX_PATH+5]; /* Overallocate for \\*.*\0 */
-    char *bufptr = namebuf;
-    Py_ssize_t len = sizeof(namebuf)-5; /* only claim to have space for MAX_PATH */
-
+    WIN32_FIND_DATAW wFileData;
+    Py_UNICODE *wnamebuf;
+    Py_ssize_t len;
     PyObject *po;
-    if (PyArg_ParseTuple(args, "U:listdir", &po)) {
-        WIN32_FIND_DATAW wFileData;
-        Py_UNICODE *wnamebuf;
-        /* Overallocate for \\*.*\0 */
-        len = PyUnicode_GET_SIZE(po);
-        wnamebuf = malloc((len + 5) * sizeof(wchar_t));
-        if (!wnamebuf) {
-            PyErr_NoMemory();
-            return NULL;
-        }
-        wcscpy(wnamebuf, PyUnicode_AS_UNICODE(po));
-        if (len > 0) {
-            Py_UNICODE wch = wnamebuf[len-1];
-            if (wch != L'/' && wch != L'\\' && wch != L':')
-                wnamebuf[len++] = L'\\';
-            wcscpy(wnamebuf + len, L"*.*");
-        }
-        if ((d = PyList_New(0)) == NULL) {
-            free(wnamebuf);
-            return NULL;
-        }
-        Py_BEGIN_ALLOW_THREADS
-        hFindFile = FindFirstFileW(wnamebuf, &wFileData);
-        Py_END_ALLOW_THREADS
-        if (hFindFile == INVALID_HANDLE_VALUE) {
-            int error = GetLastError();
-            if (error == ERROR_FILE_NOT_FOUND) {
-                free(wnamebuf);
-                return d;
-            }
-            Py_DECREF(d);
-            win32_error_unicode("FindFirstFileW", wnamebuf);
-            free(wnamebuf);
-            return NULL;
-        }
-        do {
-            /* Skip over . and .. */
-            if (wcscmp(wFileData.cFileName, L".") != 0 &&
-                wcscmp(wFileData.cFileName, L"..") != 0) {
-                v = PyUnicode_FromUnicode(wFileData.cFileName, wcslen(wFileData.cFileName));
-                if (v == NULL) {
-                    Py_DECREF(d);
-                    d = NULL;
-                    break;
-                }
-                if (PyList_Append(d, v) != 0) {
-                    Py_DECREF(v);
-                    Py_DECREF(d);
-                    d = NULL;
-                    break;
-                }
-                Py_DECREF(v);
-            }
-            Py_BEGIN_ALLOW_THREADS
-            result = FindNextFileW(hFindFile, &wFileData);
-            Py_END_ALLOW_THREADS
-            /* FindNextFile sets error to ERROR_NO_MORE_FILES if
-               it got to the end of the directory. */
-            if (!result && GetLastError() != ERROR_NO_MORE_FILES) {
-                Py_DECREF(d);
-                win32_error_unicode("FindNextFileW", wnamebuf);
-                FindClose(hFindFile);
-                free(wnamebuf);
-                return NULL;
-            }
-        } while (result == TRUE);
+    PyObject *name_stat;
 
-        if (FindClose(hFindFile) == FALSE) {
-            Py_DECREF(d);
-            win32_error_unicode("FindClose", wnamebuf);
-            free(wnamebuf);
-            return NULL;
-        }
-        free(wnamebuf);
-        return d;
-    }
-    /* Drop the argument parsing error as narrow strings
-       are also valid. */
-    PyErr_Clear();
-
-    if (!PyArg_ParseTuple(args, "et#:listdir",
-                          Py_FileSystemDefaultEncoding, &bufptr, &len))
+    if (!PyArg_ParseTuple(args, "U:listdir", &po))
         return NULL;
+
+    /* Overallocate for \\*.*\0 */
+    len = PyUnicode_GET_SIZE(po);
+    wnamebuf = malloc((len + 5) * sizeof(wchar_t));
+    if (!wnamebuf) {
+        PyErr_NoMemory();
+        return NULL;
+    }
+    wcscpy(wnamebuf, PyUnicode_AS_UNICODE(po));
     if (len > 0) {
-        char ch = namebuf[len-1];
-        if (ch != SEP && ch != ALTSEP && ch != ':')
-            namebuf[len++] = '/';
-        strcpy(namebuf + len, "*.*");
+        Py_UNICODE wch = wnamebuf[len-1];
+        if (wch != L'/' && wch != L'\\' && wch != L':')
+            wnamebuf[len++] = L'\\';
+        wcscpy(wnamebuf + len, L"*.*");
     }
-
-    if ((d = PyList_New(0)) == NULL)
+    if ((d = PyList_New(0)) == NULL) {
+        free(wnamebuf);
         return NULL;
-
+    }
     Py_BEGIN_ALLOW_THREADS
-    hFindFile = FindFirstFile(namebuf, &FileData);
+    hFindFile = FindFirstFileW(wnamebuf, &wFileData);
     Py_END_ALLOW_THREADS
     if (hFindFile == INVALID_HANDLE_VALUE) {
         int error = GetLastError();
-        if (error == ERROR_FILE_NOT_FOUND)
+        if (error == ERROR_FILE_NOT_FOUND) {
+            free(wnamebuf);
             return d;
+        }
         Py_DECREF(d);
-        return win32_error("FindFirstFile", namebuf);
+        win32_error_unicode("FindFirstFileW", wnamebuf);
+        free(wnamebuf);
+        return NULL;
     }
     do {
         /* Skip over . and .. */
-        if (strcmp(FileData.cFileName, ".") != 0 &&
-            strcmp(FileData.cFileName, "..") != 0) {
-            v = PyString_FromString(FileData.cFileName);
+        if (wcscmp(wFileData.cFileName, L".") != 0 &&
+            wcscmp(wFileData.cFileName, L"..") != 0) {
+            v = PyUnicode_FromUnicode(wFileData.cFileName, wcslen(wFileData.cFileName));
             if (v == NULL) {
                 Py_DECREF(d);
                 d = NULL;
                 break;
             }
-            if (PyList_Append(d, v) != 0) {
+            name_stat = PyTuple_Pack(2, v, find_data_to_stat(&wFileData));
+            if (name_stat == NULL) {
                 Py_DECREF(v);
                 Py_DECREF(d);
+                d = NULL;
+                break;
+            }
+            if (PyList_Append(d, name_stat) != 0) {
+                Py_DECREF(v);
+                Py_DECREF(d);
+                Py_DECREF(name_stat);
                 d = NULL;
                 break;
             }
             Py_DECREF(v);
         }
         Py_BEGIN_ALLOW_THREADS
-        result = FindNextFile(hFindFile, &FileData);
+        result = FindNextFileW(hFindFile, &wFileData);
         Py_END_ALLOW_THREADS
         /* FindNextFile sets error to ERROR_NO_MORE_FILES if
            it got to the end of the directory. */
         if (!result && GetLastError() != ERROR_NO_MORE_FILES) {
             Py_DECREF(d);
-            win32_error("FindNextFile", namebuf);
+            win32_error_unicode("FindNextFileW", wnamebuf);
             FindClose(hFindFile);
+            free(wnamebuf);
             return NULL;
         }
     } while (result == TRUE);
 
     if (FindClose(hFindFile) == FALSE) {
         Py_DECREF(d);
-        return win32_error("FindClose", namebuf);
+        win32_error_unicode("FindClose", wnamebuf);
+        free(wnamebuf);
+        return NULL;
     }
-
+    free(wnamebuf);
     return d;
 }
 
